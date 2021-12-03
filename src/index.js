078d20e990e767174318896f46ee7c5afdfe5f38
Raza Merchant <raza@idearebel.com>
2021-12-03 21:01:55 +0500

    14. File Uploads (Task App) uncompleted

--- src/index.js
+++ src/index.js
@@ -1,30 +1,76 @@
-// import npm modules
 const express = require('express')
 require('./db/mongoose')
-
-// import routers
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-    // Site Middleware for Emergency
-    console.log(req.method, req.path);
-    next()
+
+
+
+
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word Document'))
+        }
+
+        cb(undefined, true)
+
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+        // cb(undefined, false)
+    }
+})
+const errorMiddleware = (req, res, next) => {
+    throw new Error('From my middleware')
+}
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send();
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
-// use body parser
-app.use(express.json())
 
-// use body 
+
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+
+// app.use((req, res, next) => {
+//     // Site Middleware for Emergency
+//     console.log(req.method, req.path);
+//     next()
+// })
+
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
 
 // const Task = require('./models/task')
 // const User = require('./models/user')
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
+const multer = require('multer')
+
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -99,6 +101,32 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
+})
+
+const upload = multer({
+    // dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+        cb(undefined, true)
+    }
+})
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save();
+    res.send()
 })
 
 module.exports = router
